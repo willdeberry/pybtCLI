f9dc887e47bbaf85f6e14368a5d1ab9ab82a6968
Will DeBerry <willdeberry@gmail.com>
2016-03-06 15:42:14 -0500

    Version bump in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup( name = 'bjarkan',
-    version = '1.0',
+    version = '1.0.1',
     description = 'Bluetooth command line utility',
     long_description = open( 'README.rst' ).read(),
     author = 'GetWellNetwork',
