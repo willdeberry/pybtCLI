59fbd9417ff04deb7b3ef4be3ad06ac59c234140
Will DeBerry <willdeberry@gmail.com>
2016-04-08 22:51:28 -0400

    Version bump: 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup( name = 'bjarkan',
-    version = '1.0.1',
+    version = '1.1.0',
     description = 'Bluetooth command line utility',
     long_description = open( 'README.rst' ).read(),
     keywords = 'bluez bluetooth cli',
