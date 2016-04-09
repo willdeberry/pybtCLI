a69a449ed6c82ce827df1ec08431e49db2afebbe
Will DeBerry <willdeberry@gmail.com>
2016-04-08 22:19:36 -0400

    Add more information to setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ setup( name = 'bjarkan',
     version = '1.0.1',
     description = 'Bluetooth command line utility',
     long_description = open( 'README.rst' ).read(),
+    keywords = 'bluez bluetooth cli',
     author = 'GetWellNetwork',
     author_email = 'willdeberry@gmail.com',
-    license = 'BSD',
+    maintainer = 'Will DeBerry',
+    maintainer_email = 'willdeberry@gmail.com',
+    license = 'Copyright 2016 GetWellNetwork, Inc., BSD copyright and disclaimer apply',
     url = 'https://github.com/willdeberry/bjarkan',
+    download_url = 'https://github.com/willdeberry/bjarkan',
     packages = find_packages( exclude = ['contrib', 'docs', 'tests'] ),
     install_requires = [
         'pygobject>=3.18.2'
@@ -18,5 +22,14 @@ setup( name = 'bjarkan',
         'console_scripts': [
             'bjarkan = bjarkan.bjarkan:main'
         ],
-    }
+    },
+    classifiers = [
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators'
+    ]
 )
